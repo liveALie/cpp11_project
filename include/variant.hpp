7cdf1114cffd9ce4184ea5bbdd60242fbb6b73c2
liveALie <545498611@qq.com>
2018-12-03 23:42:34 +0800

    Update variant.hpp

--- include/variant.hpp
+++ include/variant.hpp
@@ -1,225 +1,302 @@
 #ifndef _C11TEST_VARIANT_HPP_
 #define _C11TEST_VARIANT_HPP_
 
-#pragma once
-
 #include <typeindex>
 #include <iostream>
+#include <type_traits>
+#include <tuple>
+#include <functional>
+#include <utility>
 
-/** 获取最大的整数 */
+//#include "function_trait.hpp"
+
+template <typename T>
+struct function_trait
+	: public function_trait<decltype(&T::operator())>
+{};
+// For generic types, directly use the result of the signature of its 'operator()'
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_trait<ReturnType(ClassType::*)(Args...) const>
+	// we specialize for pointers to member function
+{
+	enum { arity = sizeof...(Args) };
+	// arity is the number of arguments.
+
+	typedef ReturnType result_type;
+
+	template <size_t i>
+	struct arg
+	{
+		typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+		// the i-th argument is equivalent to the i-th tuple element of a tuple
+		// composed of those arguments.
+	};
+
+	typedef std::function<ReturnType(Args...)> FunType;
+	typedef std::tuple<Args...> ArgTupleType;
+};
+
+//��ȡ��������
 template <size_t arg, size_t... rest>
 struct IntegerMax;
 
 template <size_t arg>
 struct IntegerMax<arg> : std::integral_constant<size_t, arg>
 {
+	//static const size_t value = arg;
+	//enum{value = arg};
 };
 
+//��ȡ����align
 template <size_t arg1, size_t arg2, size_t... rest>
-struct IntegerMax<arg1, arg2, rest...> : std::integral_constant<size_t, arg1 >= arg2 ? IntegerMax<arg1, rest...>::value 
-    : IntegerMax<arg2, rest...>::value>
+struct IntegerMax<arg1, arg2, rest...> : std::integral_constant<size_t, arg1 >= arg2 ? IntegerMax<arg1, rest...>::value :
+	IntegerMax<arg2, rest...>::value >
 {
+	/*static const size_t value = arg1 >= arg2 ? static_max<arg1, others...>::value :
+	static_max<arg2, others...>::value;*/
 };
 
-/** 获取最大的align */
+
 template<typename... Args>
-struct MaxAlign : std::integral_constant<int, IntegerMax<std::alignment_of<Args>::value...>::value>
-{
-};
+struct MaxAlign : std::integral_constant<int, IntegerMax<std::alignment_of<Args>::value...>::value>{};
+/*
+template<typename T, typename... Args>
+struct MaxAlign : std::integral_constant<int,
+(std::alignment_of<T>::value >MaxAlign<Args...>::value ? std::alignment_of<T>::value : MaxAlign<Args...>::value) >
+{};
 
-/** 是否包含某个类型 */
-template <typename T, typename... List>
-struct Contains;
+template<typename T>
+struct MaxAlign<T> : std::integral_constant<int, std::alignment_of<T>::value >{}; */
 
-template <typename T, typename Head, typename... Rest>
+//�Ƿ����ĳ������
+template < typename T, typename... List >
+struct Contains : std::true_type {};
+
+template < typename T, typename Head, typename... Rest >
 struct Contains<T, Head, Rest...>
-	: std::conditional<std::is_same<T, Head>::value, std::true_type, Contains<T, Rest... >> ::type
+	: std::conditional< std::is_same<T, Head>::value, std::true_type, Contains<T, Rest... >> ::type{};
+
+template < typename T >
+struct Contains<T> : std::false_type{};
+
+//��ȡ��һ��T������λ��
+// Forward
+template<typename Type, typename... Types>
+struct GetLeftSize;
+
+// Declaration
+template<typename Type, typename First, typename... Types>
+struct GetLeftSize<Type, First, Types...> : GetLeftSize<Type, Types...>
 {
 };
 
-template <typename T>
-struct Contains<T> : std::false_type
+// Specialized
+template<typename Type, typename... Types>
+struct GetLeftSize<Type, Type, Types...> : std::integral_constant<int, sizeof...(Types)>
 {
+	//static const int ID = sizeof...(Types);
 };
 
-template <typename T, typename... List>
-struct IndexOf;
-
-template <typename T, typename Head, typename... Rest>
-struct IndexOf<T, Head, Rest...>
+template<typename Type>
+struct GetLeftSize<Type> : std::integral_constant<int, -1>
 {
-	enum { value = IndexOf<T, Rest...>::value + 1 };
-};
-
-template <typename T, typename... Rest>
-struct IndexOf<T, T, Rest...>
-{
-	enum { value = 0 };
-};
-
-template <typename T>
-struct IndexOf<T>
-{
-	enum{value = -1};
-};
-
-template<int index, typename... Types>
-struct At;
-
-template<int index, typename First, typename... Types>
-struct At<index, First, Types...>
-{
-	using type = typename At<index - 1, Types...>::type;
+	//static const int ID = -1;
 };
 
 template<typename T, typename... Types>
-struct At<0, T, Types...>
+struct Index : std::integral_constant<int, sizeof...(Types)-GetLeftSize<T, Types...>::value - 1>{};
+
+//����������ȡ����λ�õ�����
+// Forward declaration
+template<int index, typename... Types>
+struct IndexType;
+
+// Declaration
+template<int index, typename First, typename... Types>
+struct IndexType<index, First, Types...> : IndexType<index - 1, Types...>
 {
-	using type = T;
+};
+
+// Specialized
+template<typename First, typename... Types>
+struct IndexType<0, First, Types...>
+{
+	typedef First DataType;
+};
+
+template<typename... Args>
+struct VariantHelper;
+
+template<typename T, typename... Args>
+struct VariantHelper<T, Args...> 
+{
+	inline static void Destroy(std::type_index id, void * data)
+	{
+		if (id == std::type_index(typeid(T)))
+			//((T*) (data))->~T();
+			reinterpret_cast<T*>(data)->~T();
+		else
+			VariantHelper<Args...>::Destroy(id, data);
+	}
+
+	inline static void move(std::type_index old_t, void * old_v, void * new_v)
+	{
+		if (old_t == std::type_index(typeid(T)))
+			new (new_v)T(std::move(*reinterpret_cast<T*>(old_v)));
+		else
+			VariantHelper<Args...>::move(old_t, old_v, new_v);
+	}
+
+	inline static void copy(std::type_index old_t, const void * old_v, void * new_v)
+	{
+		if (old_t == std::type_index(typeid(T)))
+			new (new_v)T(*reinterpret_cast<const T*>(old_v));
+		else
+			VariantHelper<Args...>::copy(old_t, old_v, new_v);
+	}
+};
+
+template<>
+struct VariantHelper<>  
+{
+	inline static void Destroy(std::type_index id, void * data) {  }
+	inline static void move(std::type_index old_t, void * old_v, void * new_v) { }
+	inline static void copy(std::type_index old_t, const void * old_v, void * new_v) { }
 };
 
 template<typename... Types>
 class Variant
 {
+	typedef VariantHelper<Types...> Helper_t;
+
 	enum
 	{
 		data_size = IntegerMax<sizeof(Types)...>::value,
-		align_size = MaxAlign<Types...>::value
+		//align_size = IntegerMax<alignof(Types)...>::value
+		align_size = MaxAlign<Types...>::value //ctp����alignof, Ϊ�˼����ô˰汾
 	};
 	using data_t = typename std::aligned_storage<data_size, align_size>::type;
+
 public:
 	template<int index>
-	using IndexType = typename At<index, Types...>::type;
+	using IndexType = typename IndexType<index, Types...>::DataType;
 
-	Variant(void) :type_index_(typeid(void))
+	Variant(void) :m_typeIndex(typeid(void)), m_index(-1)
 	{
 	}
 
 	~Variant()
 	{
-		destroy(type_index_, &data_);
+		Helper_t::Destroy(m_typeIndex, &m_data);
 	}
 
-	Variant(Variant<Types...>&& old) : type_index_(old.type_index_)
+	Variant(Variant<Types...>&& old) : m_typeIndex(old.m_typeIndex)
 	{
-		move(old.type_index_, &old.data_, &data_);
+		Helper_t::move(old.m_typeIndex, &old.m_data, &m_data);
 	}
 
-	Variant(const Variant<Types...>& old) : type_index_(old.type_index_)
+	Variant(const Variant<Types...>& old) : m_typeIndex(old.m_typeIndex)
 	{
-		copy(old.type_index_, &old.data_, &data_);
+		Helper_t::copy(old.m_typeIndex, &old.m_data, &m_data);
 	}
+
 
 	Variant& operator=(const Variant& old)
 	{
-		copy(old.type_index_, &old.data_, &data_);
-		type_index_ = old.type_index_;
+		Helper_t::copy(old.m_typeIndex, &old.m_data, &m_data);
+		m_typeIndex = old.m_typeIndex;
 		return *this;
 	}
 
 	Variant& operator=(Variant&& old)
 	{
-		move(old.type_index_, &old.data_, &data_);
-		type_index_ = old.type_index_;
+		Helper_t::move(old.m_typeIndex, &old.m_data, &m_data);
+		m_typeIndex = old.m_typeIndex;
 		return *this;
 	}
-
 	template <class T,
-	class = typename std::enable_if<Contains<typename std::decay<T>::type, Types...>::value>::type>
-		Variant(T&& value) : type_index_(typeid(void))
+	class = typename std::enable_if<Contains<typename std::remove_reference<T>::type, Types...>::value>::type>
+		Variant(T&& value) : m_typeIndex(typeid(void))
 	{
-			destroy(type_index_, &data_);
-			typedef typename std::decay<T>::type U;
-			new(&data_) U(std::forward<T>(value));
-			type_index_ = std::type_index(typeid(U));
-	}
+			Helper_t::Destroy(m_typeIndex, &m_data);
+			typedef typename std::remove_reference<T>::type U;
+			new(&m_data) U(std::forward<T>(value));
+			m_typeIndex = std::type_index(typeid(T));
+		}
 
 	template<typename T>
-	bool is() const
+	bool Is() const
 	{
-		return (type_index_ == std::type_index(typeid(T)));
+		return (m_typeIndex == std::type_index(typeid(T)));
 	}
 
 	bool Empty() const
 	{
-		return type_index_ == std::type_index(typeid(void));
+		return m_typeIndex == std::type_index(typeid(void));
 	}
 
-	std::type_index type() const
+	std::type_index Type() const
 	{
-		return type_index_;
+		return m_typeIndex;
 	}
 
 	template<typename T>
-	typename std::decay<T>::type& get()
+	typename std::decay<T>::type& Get()
 	{
 		using U = typename std::decay<T>::type;
-		if (!is<U>())
+		if (!Is<U>())
 		{
-			std::cout << typeid(U).name() << " is not defined. " 
-                << "current type is " << type_index_.name() << std::endl;
-			throw std::bad_cast{};
+			std::cout << typeid(U).name() << " is not defined. " << "current type is " <<
+				m_typeIndex.name() << std::endl;
+			throw std::bad_cast();
 		}
 
-		return *(U*)(&data_);
+		return *(U*)(&m_data);
 	}
 
-	template <typename T>
-	int indexOf()
+	template<typename T>
+	int GetIndexOf()
 	{
-		return IndexOf<T, Types...>::value;
+		return Index<T, Types...>::value;
+	}
+
+	template<typename F>
+	void Visit(F&& f)
+	{
+        typedef typename std::tuple_element<0, typename function_trait<F>::ArgTupleType>::type T;
+		//typedef typename function_trait<F>::ArgTupleType;
+		//using T = typename function_trait<F>::(typename arg<0>)::type;
+        if (Is<T>())
+			f(Get<T>());
+	}
+
+	template<typename F, typename... Rest>
+	void Visit(F&& f, Rest&&... rest)
+	{
+		//using T = typename function_trait<F>::(typename arg<0>)::type;
+        //typedef typename function_trait<F>::arg<0>::type T;
+        typedef typename std::tuple_element<0, typename function_trait<F>::ArgTupleType>::type T;
+		if (Is<T>())
+			Visit(std::forward<F>(f));
+		else
+			Visit(std::forward<Rest>(rest)...);
 	}
 
 	bool operator==(const Variant& rhs) const
 	{
-		return type_index_ == rhs.type_index_;
+		return m_typeIndex == rhs.m_typeIndex;
 	}
 
 	bool operator<(const Variant& rhs) const
 	{
-		return type_index_ < rhs.type_index_;
+		return m_typeIndex < rhs.m_typeIndex;
 	}
 
 private:
-	void destroy(const std::type_index& index, void *buf)
-	{
-        [](Types&&...){}((destroy0<Types>(index, buf), 0)...);
-	}
-
-	template<typename T>
-	void destroy0(const std::type_index& id, void *data)
-	{
-		if (id == std::type_index(typeid(T)))
-			reinterpret_cast<T*>(data)->~T();
-	}
-
-	void move(const std::type_index& old_t, void *old_v, void *new_v) 
-	{
-        [](Types&&...){}((move0<Types>(old_t, old_v, new_v), 0)...);
-	}
-
-	template<typename T>
-	void move0(const std::type_index& old_t, void *old_v, void *new_v)
-	{
-		if (old_t == std::type_index(typeid(T)))
-			new (new_v)T(std::move(*reinterpret_cast<T*>(old_v)));
-	}
-
-	void copy(const std::type_index& old_t, const void *old_v, void *new_v)
-	{
-        [](Types&&...){}((copy0<Types>(old_t, old_v, new_v), 0)...);
-	}
-
-	template<typename T>
-	void copy0(const std::type_index& old_t, const void *old_v, void *new_v)
-	{
-		if (old_t == std::type_index(typeid(T)))
-			new (new_v)T(*reinterpret_cast<const T*>(old_v));
-	}
-
-private:
-	data_t data_;
-	std::type_index type_index_;
+	data_t m_data;
+	std::type_index m_typeIndex;  //����ID
+    int m_index;
 };
 
 #endif // _C11TEST_VARIANT_HPP_
