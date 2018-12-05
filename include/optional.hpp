b36253e3790becd7f16630686018e0d78ad05761
liveALie <545498611@qq.com>
2018-12-05 10:15:11 +0800

    Create optional.hpp

---
+++ include/optional.hpp
@@ -0,0 +1,102 @@
+#ifndef _C11TEST_OPTIONAL_HPP_
+#define _C11TEST_OPTIONAL_HPP_
+
+#include <type_traits>
+
+template<typename T>
+class Optional{
+    using data_t = typename std::aligned_storage<sizeof(T),std::alignment_of<T>::value>::type;
+public:
+    Optional(){}
+    Optional(const T& v)
+    {
+        Create(v);
+    }
+
+    Optional(const Optional& other)
+    {
+        if(other.IsInit()){
+            Assign(other);
+        }
+    }
+
+    Optional& operator=(const Optional& other)
+    {
+        if(this == &other)
+            return *this;
+        if(other.IsInit()){
+            Assign(other);
+        }else{
+            Destroy();
+        }
+        return *this;
+    }
+
+    ~Optional()
+    {
+        Destroy();
+    }
+
+    template<typename... Args>
+    void Emplace(Args&&... args)
+    {
+        Destroy();
+        Create(std::forward<Args>(args)...);
+    }
+
+    bool IsInit()const
+    {
+        return has_init_;
+    }
+
+    explicit operator bool()const//可以直接if(option){}
+    {
+        return IsInit();
+    }
+
+    T const& operator*()const
+    {
+        if(IsInit()){
+            return *((T*)(&data_));
+        }
+        throw std::logic_error("is not init.");
+    }
+
+private:
+    template<typename... Args>
+    void Create(Args&&... args)
+    {
+        new (&data_) T(std::forward<Args>(args)...);
+        has_init_ = true;
+    }
+
+    void Destroy()
+    {
+        if(has_init_){
+            has_init_ = false;
+            ((T*)(&data_))->~T();
+        }
+    }
+
+    void Assign(const Optional& other)
+    {
+        if(other.IsInit()){
+            Copy(other.data_);
+            has_init_ = true;
+        }else{
+            Destroy();
+        }
+    }
+
+    void Copy(const data_t& val)
+    {
+        Destroy();
+        new (&data_) T((*(T*)(&val)));
+    }
+private:
+    bool has_init_ = false;
+    data_t data_;
+
+};
+
+#endif // _C11TEST_OPTIONAL_HPP_
