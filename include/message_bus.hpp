5b58083709a3b5e2bf200b57d2cabea8d14d8e70
liveALie <545498611@qq.com>
2018-12-06 20:19:53 +0800

    Update message_bus.hpp

--- include/message_bus.hpp
+++ include/message_bus.hpp
@@ -11,13 +11,15 @@
 
 class MessageBus : NonCopyable{
 public:
+    //注册要接受的topic和消息接收器
     template<typename F>
     void Attach(F&& f,const std::string& topic = "")
     {
+        //转换成统一的消息接收者类型。std::function<T>包装器类型。
         auto func = to_function(std::forward<F>(f));
         Add(topic,std::move(func));
     }
-
+    //发送消息，通知消息，无参的，接收器的返回类型为R
     template<typename R>
     void SendReq(const std::string& topic)
     {
@@ -31,6 +33,7 @@ public:
         }
     }
 
+    //接收器的返回类型为R，参数类型为Args
     template<typename R,typename... Args>
     void SendReq(Args&&... args,const std::string& topic = "")
     {
@@ -44,23 +47,26 @@ public:
         }
     }
     
+    //删除订阅者，批量删除
     template<typename R,typename... Args>
     void Remove(const std::string& topic = "")
     {
         using function_type = std::function<R(Args...)>;
         std::string msg_type = topic + typeid(function_type).name();
         auto range = map_.equal_range(msg_type);
-        map_.erase(range.first,range.second);
+        map_.erase(range.first,range.second);//这不是把所有订阅者都删了吗？
     }
 
 private:
     template<typename Function>
     void Add(const std::string& topic,Function&& f)
     {
+        //topic + 类型签名 为key值。
         std::string msg_type = topic + typeid(Function).name();
         map_.emplace(msg_type,std::forward<Function>(f));
     }
 private:
+    //用Any类型擦除，用multimap
     std::multimap<std::string,Any> map_;
     typedef std::multimap<std::string,Any>::iterator Iterater;
 };
