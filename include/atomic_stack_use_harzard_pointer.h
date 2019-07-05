a28221c14290027e2d525702b4f1278821e702d7
shanchao <scding@nullmax.ai>
2019-07-05 10:46:01 +0800

    modify

--- include/atomic_stack_use_harzard_pointer.h
+++ include/atomic_stack_use_harzard_pointer.h
@@ -104,7 +104,7 @@ void delete_nodes_with_no_hazards() {
   }
 }
 
-template <typename T> class lock_free_stack {
+template <typename T> class LockFreeStackUseHarzardPointer {
 public:
   void push(T const &data) {
     node *const new_node = new node(data);
@@ -113,20 +113,9 @@ public:
       ;
   }
 
-  std::shared_ptr<T> pop_use_thread_reference_count() {
-    ++threads_in_pop; // 2 在做事之前，计数值加1
-    node *old_head = head.load();
-    while (old_head && !head.compare_exchange_weak(old_head, old_head->next))
-      ;
-    std::shared_ptr<T> res;
-    if (old_head) {
-      res.swap(old_head->data); // 3 回收删除的节点
-    }
-    try_reclaim(old_head); // 4 从节点中直接提取数据，而非拷贝指针
-    return res;
-  }
+  
   //通过存储读取每个结点的线程数来实现：以精确识别出哪些结点正在被引用以及何时没有线程读取这些结点，那么就可以删除此结点。
-  std::shared_ptr<T> pop_use_hazard_pointer() {
+  std::shared_ptr<T> pop() {
     std::atomic<void *> &hp = get_hazard_pointer_for_current_thread();
     node *old_head = head.load();
     do {
@@ -153,53 +142,6 @@ public:
       delete_nodes_with_no_hazards(); // 6
     }
     return res;
-  }
-
-private:
-  static void delete_nodes(node *nodes) {
-    while (nodes) {
-      node *next = nodes->next;
-      delete nodes;
-      nodes = next;
-    }
-  }
-
-  void try_reclaim(node *old_head) {
-    if (threads_in_pop == 1) // 1
-    {
-      node *nodes_to_delete =
-          to_be_deleted.exchange(nullptr); // 2 声明“可删除”列表
-      if (!--threads_in_pop) // 3 是否只有一个线程调用pop()？
-      {
-        delete_nodes(nodes_to_delete); // 4
-      } else if (nodes_to_delete)      // 5
-      {
-        chain_pending_nodes(nodes_to_delete); // 6
-      }
-      delete old_head; // 7
-    } else {
-      chain_pending_node(old_head); // 8
-      --threads_in_pop;
-    }
-  }
-  void chain_pending_nodes(node *nodes) {
-    node *last = nodes;
-    while (node *const next = last->next) // 9 让next指针指向链表的末尾
-    {
-      last = next;
-    }
-    chain_pending_nodes(nodes, last);
-  }
-
-  void chain_pending_nodes(node *first, node *last) {
-    last->next = to_be_deleted; // 10
-    while (!to_be_deleted
-                .compare_exchange_weak( // 11 用循环来保证last->next的正确性
-                    last->next, first))
-      ;
-  }
-  void chain_pending_node(node *n) {
-    chain_pending_nodes(n, n); // 12
   }
 
 private:
