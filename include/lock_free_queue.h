798f642aa5d51bae4cd03e531fd45f728eb299be
shanchao <scding@nullmax.ai>
2019-07-16 19:18:17 +0800

    modify

--- include/lock_free_queue.h
+++ include/lock_free_queue.h
@@ -5,24 +5,24 @@
 
 template <typename T> class LockFreeQueue {
 
-  struct node;
-  struct counted_node_ptr {
-    int external_count;
-    node *ptr;
+  struct Node;
+  struct CountedNodePtr {
+    int external_count; //节点外部计数
+    Node *ptr;
   };
 
-  struct node_counter {
-    unsigned internal_count : 30;
-    unsigned external_counters : 2; // 2
+  struct NodeCounter {
+    unsigned internal_count : 30;   //节点内部计数
+    unsigned external_counters : 2; //节点外部计数器个数
   };
 
-  struct node {
+  struct Node {
     std::atomic<T *> data;
-    std::atomic<node_counter> count; // 3
-    std::atomic<counted_node_ptr> next;
+    std::atomic<NodeCounter> count; // 3
+    std::atomic<CountedNodePtr> next;
 
-    node() {
-      node_counter new_count;
+    Node() {
+      NodeCounter new_count;
       new_count.internal_count = 0;
       new_count.external_counters = 2; // 4
       count.store(new_count);
@@ -30,9 +30,10 @@ template <typename T> class LockFreeQueue {
       next.ptr = nullptr;
       next.external_count = 0;
     }
-    void release_ref() {
-      node_counter old_counter = count.load(std::memory_order_relaxed);
-      node_counter new_counter;
+
+    void ReleaseRef() {
+      NodeCounter old_counter = count.load(std::memory_order_relaxed);
+      NodeCounter new_counter;
       do {
         new_counter = old_counter;
         --new_counter.internal_count;          // 1
@@ -46,63 +47,63 @@ template <typename T> class LockFreeQueue {
   };
 
 public:
-  void push(T new_value) {
+  void Push(T new_value) {
     std::unique_ptr<T> new_data(new T(new_value));
-    counted_node_ptr new_next;
-    new_next.ptr = new node;
+    CountedNodePtr new_next;
+    new_next.ptr = new Node;
     new_next.external_count = 1;
-    counted_node_ptr old_tail = tail.load();
+    CountedNodePtr old_tail = tail_.load();
 
     for (;;) {
-      increase_external_count(tail, old_tail);
+      IncreaseExternalCount(tail_, old_tail);
 
       T *old_data = nullptr;
       if (old_tail.ptr->data.compare_exchange_strong( // 6
               old_data, new_data.get())) {
-        counted_node_ptr old_next = {0};
+        CountedNodePtr old_next = {0};
         if (!old_tail.ptr->next.compare_exchange_strong( // 7
                 old_next, new_next)) {
           delete new_next.ptr; // 8
           new_next = old_next; // 9
         }
-        set_new_tail(old_tail, new_next);
+        SetNewTail(old_tail, new_next);
         new_data.release();
         break;
       } else // 10
       {
-        counted_node_ptr old_next = {0};
+        CountedNodePtr old_next = {0};
         if (old_tail.ptr->next.compare_exchange_strong( // 11
                 old_next, new_next)) {
           old_next = new_next;     // 12
-          new_next.ptr = new node; // 13
+          new_next.ptr = new Node; // 13
         }
-        set_new_tail(old_tail, old_next); // 14
+        SetNewTail(old_tail, old_next); // 14
       }
     }
   }
 
-  std::unique_ptr<T> pop() {
-    counted_node_ptr old_head = head.load(std::memory_order_relaxed);
+  std::unique_ptr<T> Pop() {
+    CountedNodePtr old_head = head_.load(std::memory_order_relaxed);
     for (;;) {
-      increase_external_count(head, old_head);
-      node *const ptr = old_head.ptr;
-      if (ptr == tail.load().ptr) {
+      IncreaseExternalCount(head_, old_head);
+      Node *const ptr = old_head.ptr;
+      if (ptr == tail_.load().ptr) {
         return std::unique_ptr<T>();
       }
-      counted_node_ptr next = ptr->next.load(); // 2
-      if (head.compare_exchange_strong(old_head, next)) {
+      CountedNodePtr next = ptr->next.load(); // 2
+      if (head_.compare_exchange_strong(old_head, next)) {
         T *const res = ptr->data.exchange(nullptr);
-        free_external_counter(old_head);
+        FreeExternalCounter(old_head);
         return std::unique_ptr<T>(res);
       }
-      ptr->release_ref();
+      ptr->ReleaseRef();
     }
   }
 
 private:
-  static void increase_external_count(std::atomic<counted_node_ptr> &counter,
-                                      counted_node_ptr &old_counter) {
-    counted_node_ptr new_counter;
+  static void IncreaseExternalCount(std::atomic<CountedNodePtr> &counter,
+                                    CountedNodePtr &old_counter) {
+    CountedNodePtr new_counter;
     do {
       new_counter = old_counter;
       ++new_counter.external_count;
@@ -113,12 +114,12 @@ private:
     old_counter.external_count = new_counter.external_count;
   }
 
-  static void free_external_counter(counted_node_ptr &old_node_ptr) {
-    node *const ptr = old_node_ptr.ptr;
+  static void FreeExternalCounter(CountedNodePtr &old_node_ptr) {
+    Node *const ptr = old_node_ptr.ptr;
     int const count_increase = old_node_ptr.external_count - 2;
 
-    node_counter old_counter = ptr->count.load(std::memory_order_relaxed);
-    node_counter new_counter;
+    NodeCounter old_counter = ptr->count.load(std::memory_order_relaxed);
+    NodeCounter new_counter;
     do {
       new_counter = old_counter;
       --new_counter.external_counters;              // 1
@@ -132,19 +133,19 @@ private:
     }
   }
 
-  void set_new_tail(counted_node_ptr &old_tail, // 1
-                    counted_node_ptr const &new_tail) {
-    node *const current_tail_ptr = old_tail.ptr;
-    while (!tail.compare_exchange_weak(old_tail, new_tail) && // 2
+  void SetNewTail(CountedNodePtr &old_tail, // 1
+                  CountedNodePtr const &new_tail) {
+    Node *const current_tail_ptr = old_tail.ptr;
+    while (!tail_.compare_exchange_weak(old_tail, new_tail) && // 2
            old_tail.ptr == current_tail_ptr)
       ;
     if (old_tail.ptr == current_tail_ptr) // 3
-      free_external_counter(old_tail);    // 4
+      FreeExternalCounter(old_tail);      // 4
     else
-      current_tail_ptr->release_ref(); // 5
+      current_tail_ptr->ReleaseRef(); // 5
   }
 
 private:
-  std::atomic<counted_node_ptr> head;
-  std::atomic<counted_node_ptr> tail; // 1
+  std::atomic<CountedNodePtr> head_;
+  std::atomic<CountedNodePtr> tail_; // 1
 };
